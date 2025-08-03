994c72fadb50e08c73b0141918e1f31f823c265b
lebao3105 <bao12345yocoo@gmail.com>
2025-08-03 12:22:49 +0700

    Add more controls

--- Shared/Views/TransferItem.xaml.cs
+++ Shared/Views/TransferItem.xaml.cs
@@ -5,11 +5,6 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-
-#if WINDOWS_PHONE
-using System.Windows;
-using System.Windows.Controls;
-#else
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -17,22 +12,16 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-#endif
 
 // The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
 namespace Shared.Views
 {
-    public sealed partial class SendPage : UserControl
+    public sealed partial class TransferItem : UserControl
     {
-        public SendPage()
+        public TransferItem()
         {
             this.InitializeComponent();
-            Loaded += SendPage_Loaded;
-        }
-
-        private void SendPage_Loaded(object sender, RoutedEventArgs e)
-        {
         }
     }
 }
